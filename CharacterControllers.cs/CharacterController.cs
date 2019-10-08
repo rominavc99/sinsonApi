7a32063238e8b923add0959e3a0063446fb38199
rominavc99 <procrom1999@gmail.com>
2019-10-08 13:54:15 -0700

    mas personajes

--- CharacterControllers.cs/CharacterController.cs
+++ CharacterControllers.cs/CharacterController.cs
@@ -19,13 +19,42 @@ namespace api_simpsons.Controllers
                 Name = "Homero",
                 LastName = "Simposon",
                 SecondName = "Jay",
-                Age = 24
+                Description = "Esposo de Marge y padre de Bart, Lisa y Maggie Simpson.",
+                Age = 24,
+                Photo = "http://pluspng.com/img-png/marge-simpson-hd-png-marge-simpson-2-png-1220.png"
             },
+
+            new Character{
+                Name = "Margory Simpson", 
+                Age = 36, 
+                Description = "Esposa de Homero, Madre de la familia Simpson.",
+                Photo = "http://pluspng.com/img-png/marge-simpson-hd-png-marge-simpson-2-png-1220.png"         
+            },
+
             new Character{
                 Name = "Bart",
                 LastName = "Simposon",
-                Age = 10
+                Age = 10,
+                Description = "Hermano de Lisa y Maggie Simpson.",
+                Photo = "https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png"        
             },
+
+            new Character{
+                Name = "Lisa", 
+                LastName = "Simpson",
+                Age = 9, 
+                Description = "Hermana de Bart y Maggie Simpsons.",
+                Photo = "https://upload.wikimedia.org/wikipedia/en/thumb/e/ec/Lisa_Simpson.png/220px-Lisa_Simpson.png"         
+            },
+            new Character{
+                Name = "Maggie", 
+                LastName = "Simpson",
+                Age = 2, 
+                Description = "Hermana de Bart y Lisa Simpson.",
+                Photo = "https://upload.wikimedia.org/wikipedia/en/thumb/9/9d/Maggie_Simpson.png/220px-Maggie_Simpson.png"         
+            },
+
+
         };
 
         [HttpGet("{id}")]
